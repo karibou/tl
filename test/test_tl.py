b444dd91e38305978a4fd82fafbdb664705dab1d
Louis Bouchard <louis.bouchard@canonical.com>
2016-01-11 12:48:43 +0100

    [test] Adapt to new categories
    Change tests according to new categories standards.
    Fix PEP8 errors and change some output variable for
    better readability
    
    Signed-off-by: Louis Bouchard <louis.bouchard@canonical.com>

--- test/test_tl.py
+++ test/test_tl.py
@@ -11,55 +11,65 @@ class TlTest(unittest.TestCase):
         # Prepare timelog file with existing tasks
         with open(tl.LogFile, 'w') as timelog:
             timelog.write("2015-03-13 13:21: lunch** :\n")
-            timelog.write("2015-03-13 16:34: Upstream Ubuntu :  lp1415880\n")
+            timelog.write("2015-03-13 16:34: Launchpad & Public :  lp1415880\n")
             timelog.write("2015-03-13 17:45: L3 / L3 support :  SF7-openafs\n")
             timelog.write("2015-03-16 08:41: arrived\n")
             timelog.write("2015-03-16 09:57: Personal management :  email\n")
             timelog.write("2015-03-16 11:07: L3 / L3 support :  SF7-openafs\n")
             timelog.write("2015-03-16 11:11: L3 / L3 support :  SF801-kdump\n")
-            timelog.write("2015-03-17 11:01: Skills building :  python\n")
-            timelog.write("2015-03-17 11:02: Skills building :  ruby\n")
-            timelog.write("2015-03-17 11:03: Skills building :  c\n")
-            timelog.write("2015-03-17 11:04: Skills building :  apl\n")
-            timelog.write("2015-03-17 11:05: Skills building :  perl\n")
-            timelog.write("2015-03-17 11:06: Skills building :  go\n")
-            timelog.write("2015-03-17 11:07: Skills building :  java\n")
-            timelog.write("2015-03-17 11:08: Skills building :  nodejs\n")
-            timelog.write("2015-03-17 11:09: Skills building :  erlang\n")
-            timelog.write("2015-03-17 11:10: Skills building :  basic\n")
-            timelog.write("2015-03-17 11:11: Skills building :  pascal\n")
-            timelog.write("2015-03-18 11:10: Internal meetings : maas\n")
-            timelog.write("2015-03-18 11:11: Internal meetings : juju\n")
-            timelog.write("2015-03-18 11:12: Internal meetings : supp\n")
-            timelog.write("2015-03-18 11:13: Internal meetings : team\n")
-            timelog.write("2015-03-18 11:14: Internal meetings : mgr\n")
-            timelog.write("2015-03-18 11:15: Internal meetings : foundation\n")
-            timelog.write("2015-03-18 11:16: Internal meetings : server\n")
-            timelog.write("2015-03-18 11:17: Internal meetings : sabdfl\n")
-            timelog.write("2015-03-18 11:18: Internal meetings : kernel\n")
-            timelog.write("2015-03-18 11:19: Internal meetings : mom\n")
-            timelog.write("2015-03-19 11:10: Upstream : maas\n")
-            timelog.write("2015-03-19 11:11: Upstream : juju\n")
-            timelog.write("2015-03-19 11:12: Upstream : supp\n")
-            timelog.write("2015-03-19 11:13: Upstream : team\n")
-            timelog.write("2015-03-19 11:14: Upstream : mgr\n")
-            timelog.write("2015-03-19 11:15: Upstream : foundation\n")
-            timelog.write("2015-03-19 11:16: Upstream : server\n")
-            timelog.write("2015-03-19 11:17: Upstream : sabdfl\n")
-            timelog.write("2015-03-19 11:18: Upstream : kernel\n")
-            timelog.write("2015-03-19 11:19: Upstream : mom\n")
-            timelog.write("2015-03-19 11:20: Upstream : rsyslog\n")
-            timelog.write("2015-03-19 11:21: Upstream : sleep\n")
-            timelog.write("2015-03-19 11:22: Upstream : binutils\n")
-            timelog.write("2015-03-19 11:23: Upstream : awk\n")
-            timelog.write("2015-03-19 11:24: Upstream : grep\n")
-            timelog.write("2015-03-19 11:25: Upstream : make\n")
-            timelog.write("2015-03-19 11:26: Upstream : autotools\n")
-            timelog.write("2015-03-19 11:27: Upstream : autoconf\n")
-            timelog.write("2015-03-19 11:28: Upstream : ifenslave\n")
-            timelog.write("2015-03-19 11:29: Upstream : true\n")
-            timelog.write("2015-03-19 11:30: Upstream : false\n")
-            timelog.write("2015-03-19 11:31: Upstream : what\n")
+            timelog.write(("2015-03-17 11:01: Mentoring / Edu / Training :  "
+                          "python\n"))
+            timelog.write(("2015-03-17 11:02: Mentoring / Edu / Training :  "
+                          "ruby\n"))
+            timelog.write("2015-03-17 11:03: Mentoring / Edu / Training :  c\n")
+            timelog.write(("2015-03-17 11:04: Mentoring / Edu / Training :  "
+                          "apl\n"))
+            timelog.write(("2015-03-17 11:05: Mentoring / Edu / Training :  "
+                          "perl\n"))
+            timelog.write(("2015-03-17 11:06: Mentoring / Edu / Training :  "
+                           "go\n"))
+            timelog.write(("2015-03-17 11:07: Mentoring / Edu / Training :  "
+                           "java\n"))
+            timelog.write(("2015-03-17 11:08: Mentoring / Edu / Training :  "
+                           "nodejs\n"))
+            timelog.write(("2015-03-17 11:09: Mentoring / Edu / Training :  "
+                           "erlang\n"))
+            timelog.write(("2015-03-17 11:10: Mentoring / Edu / Training :  "
+                           "basic\n"))
+            timelog.write(("2015-03-17 11:11: Mentoring / Edu / Training :  "
+                           "pascal\n"))
+            timelog.write("2015-03-18 11:10: Meetings : maas\n")
+            timelog.write("2015-03-18 11:11: Meetings : juju\n")
+            timelog.write("2015-03-18 11:12: Meetings : supp\n")
+            timelog.write("2015-03-18 11:13: Meetings : team\n")
+            timelog.write("2015-03-18 11:14: Meetings : mgr\n")
+            timelog.write("2015-03-18 11:15: Meetings : foundation\n")
+            timelog.write("2015-03-18 11:16: Meetings : server\n")
+            timelog.write("2015-03-18 11:17: Meetings : sabdfl\n")
+            timelog.write("2015-03-18 11:18: Meetings : kernel\n")
+            timelog.write("2015-03-18 11:19: Meetings : mom\n")
+            timelog.write("2015-03-19 11:10: Launchpad & Public : maas\n")
+            timelog.write("2015-03-19 11:11: Launchpad & Public : juju\n")
+            timelog.write("2015-03-19 11:12: Launchpad & Public : supp\n")
+            timelog.write("2015-03-19 11:13: Launchpad & Public : team\n")
+            timelog.write("2015-03-19 11:14: Launchpad & Public : mgr\n")
+            timelog.write("2015-03-19 11:15: Launchpad & Public : foundation\n")
+            timelog.write("2015-03-19 11:16: Launchpad & Public : server\n")
+            timelog.write("2015-03-19 11:17: Launchpad & Public : sabdfl\n")
+            timelog.write("2015-03-19 11:18: Launchpad & Public : kernel\n")
+            timelog.write("2015-03-19 11:19: Launchpad & Public : mom\n")
+            timelog.write("2015-03-19 11:20: Launchpad & Public : rsyslog\n")
+            timelog.write("2015-03-19 11:21: Launchpad & Public : sleep\n")
+            timelog.write("2015-03-19 11:22: Launchpad & Public : binutils\n")
+            timelog.write("2015-03-19 11:23: Launchpad & Public : awk\n")
+            timelog.write("2015-03-19 11:24: Launchpad & Public : grep\n")
+            timelog.write("2015-03-19 11:25: Launchpad & Public : make\n")
+            timelog.write("2015-03-19 11:26: Launchpad & Public : autotools\n")
+            timelog.write("2015-03-19 11:27: Launchpad & Public : autoconf\n")
+            timelog.write("2015-03-19 11:28: Launchpad & Public : ifenslave\n")
+            timelog.write("2015-03-19 11:29: Launchpad & Public : true\n")
+            timelog.write("2015-03-19 11:30: Launchpad & Public : false\n")
+            timelog.write("2015-03-19 11:31: Launchpad & Public : what\n")
 
     @classmethod
     def tearDownClass(letest):
@@ -89,18 +99,39 @@ class TlTest(unittest.TestCase):
         line = self._get_last_log_line().strip()
         self.assertTrue(line.endswith('lunch***'))
 
-    def test_help(self):
-        '''testing ? to get help '''
-        self.assertEqual(tl.show_help(), ("Categories : ['comm', 'know', 'meet', "
-                                          "'pers', 'skill', 'team', 'ua', "
-                                          "'ubu', 'up']"))
+    def test_help_prettytable(self):
+        '''testing ? to get help when prettytable is installed'''
+        tl.show_help()
+        output = sys.stdout.getvalue().strip()
+        self.assertEqual(output, """+-------+-----------------------------+
+| Key   | Description                 |
++-------+-----------------------------+
+| charm | Charm Devel                 |
+| comm  | Community Involvment        |
+| doc   | Documentation / KB creation |
+| fan   | Fan development             |
+| ib    | Mellanox related            |
+| is    | IS bug work                 |
+| kb    | Knowledge base Work         |
+| lp    | Launchpad & Public          |
+| meet  | Meetings                    |
+| pers  | Personal management         |
+| qe    | QE                          |
+| seg   | SEG related activities      |
+| svvp  | SVVP/Virtio dev             |
+| train | Mentoring / Edu / Training  |
+| ua    | L3 / L3 support             |
+| z     | Mainframe related           |
++-------+-----------------------------+""")
 
     def test_select_tasks_out_of_range(self):
         '''testing category only and giving out of range answer'''
         with patch('builtins.input', return_value='3'):
             tl.select_tasks('ua')
             output = sys.stdout.getvalue().strip()
-            self.assertEqual(output, '1) SF801-kdump\n2) SF7-openafs\nInvalid task number')
+            self.assertEqual(output, """1) SF801-kdump
+2) SF7-openafs
+Invalid task number""")
 
     def test_select_tasks_select_0(self):
         '''testing category only and answering 0'''
@@ -128,57 +159,129 @@ class TlTest(unittest.TestCase):
         with patch('builtins.input', return_value='10'):
             self.assertEqual(tl.select_tasks('meet'), ('meet', 'maas'))
             output = sys.stdout.getvalue().strip()
-            self.assertEqual(output, '1) mom\n2) kernel\n3) sabdfl\n4) server\n5) foundation\n6) mgr\n7) team\n8) supp\n9) juju\n10) maas','maas')
+            self.assertEqual(output, """1) mom
+2) kernel
+3) sabdfl
+4) server
+5) foundation
+6) mgr
+7) team
+8) supp
+9) juju
+10) maas""", 'maas')
 
     def test_category_with_exactly_10_items_no_selection(self):
         '''testing category with 10 entries and no selection'''
         with patch('builtins.input', return_value=''):
             self.assertEqual(tl.select_tasks('meet'), (None, None))
             output = sys.stdout.getvalue().strip()
-            self.assertEqual(output, '1) mom\n2) kernel\n3) sabdfl\n4) server\n5) foundation\n6) mgr\n7) team\n8) supp\n9) juju\n10) maas')
+            self.assertEqual(output, """1) mom
+2) kernel
+3) sabdfl
+4) server
+5) foundation
+6) mgr
+7) team
+8) supp
+9) juju
+10) maas""")
 
     def test_category_with_12_items(self):
         '''testing category with 12 entries and selecting first task'''
         with patch('builtins.input', return_value='1'):
-            self.assertEqual(tl.select_tasks('skill'), ('skill', 'pascal'))
+            self.assertEqual(tl.select_tasks('train'), ('train', 'pascal'))
             output = sys.stdout.getvalue().strip()
-            self.assertEqual(output, '1) pascal\n2) basic\n3) erlang\n4) nodejs\n5) java\n6) go\n7) perl\n8) apl\n9) c\n10) ruby')
+            self.assertEqual(output, """1) pascal
+2) basic
+3) erlang
+4) nodejs
+5) java
+6) go
+7) perl
+8) apl
+9) c
+10) ruby""")
 
     def test_category_with_12_items_no_selection(self):
         '''testing category with 12 entries and not selecting task'''
         with patch('builtins.input', return_value=''):
-            self.assertEqual(tl.select_tasks('skill'), (None, None))
+            self.assertEqual(tl.select_tasks('train'), (None, None))
             output = sys.stdout.getvalue().strip()
-            self.assertEqual(output, '1) pascal\n2) basic\n3) erlang\n4) nodejs\n5) java\n6) go\n7) perl\n8) apl\n9) c\n10) ruby\n11) python')
+            self.assertEqual(output, """1) pascal
+2) basic
+3) erlang
+4) nodejs
+5) java
+6) go
+7) perl
+8) apl
+9) c
+10) ruby
+11) python""")
 
     def test_category_with_12_items_last_selection(self):
         '''testing category with 12 entries and selecting first task'''
         with patch('builtins.input', return_value='11'):
-            self.assertEqual(tl.select_tasks('skill'), ('skill', 'python'))
+            self.assertEqual(tl.select_tasks('train'), ('train', 'python'))
             output = sys.stdout.getvalue().strip()
-            self.assertEqual(output, '1) pascal\n2) basic\n3) erlang\n4) nodejs\n5) java\n6) go\n7) perl\n8) apl\n9) c\n10) ruby')
+            self.assertEqual(output, """1) pascal
+2) basic
+3) erlang
+4) nodejs
+5) java
+6) go
+7) perl
+8) apl
+9) c
+10) ruby""")
 
     def test_category_with_31_items(self):
         '''testing category with 31 entries and selecting first task'''
         with patch('builtins.input', return_value='1'):
-            self.assertEqual(tl.select_tasks('up'), ('up', 'what'))
+            self.assertEqual(tl.select_tasks('lp'), ('lp', 'what'))
             output = sys.stdout.getvalue().strip()
-            self.assertEqual(output, '1) what\n2) false\n3) true\n4) ifenslave\n5) autoconf\n6) autotools\n7) make\n8) grep\n9) awk\n10) binutils')
+            self.assertEqual(output, """1) what
+2) false
+3) true
+4) ifenslave
+5) autoconf
+6) autotools
+7) make
+8) grep
+9) awk
+10) binutils""")
 
     def test_category_with_31_items_task_11(self):
         '''testing category with 31 entries and selecting eleventh task'''
         with patch('builtins.input', return_value='11'):
-            self.assertEqual(tl.select_tasks('up'), ('up', 'sleep'))
+            self.assertEqual(tl.select_tasks('lp'), ('lp', 'sleep'))
             output = sys.stdout.getvalue().strip()
-            self.assertEqual(output, '1) what\n2) false\n3) true\n4) ifenslave\n5) autoconf\n6) autotools\n7) make\n8) grep\n9) awk\n10) binutils')
+            self.assertEqual(output, """1) what
+2) false
+3) true
+4) ifenslave
+5) autoconf
+6) autotools
+7) make
+8) grep
+9) awk
+10) binutils""")
 
     def test_category_with_31_items_task_21(self):
         '''testing category with 31 entries and selecting twenty-first task'''
         with patch('builtins.input', return_value='21'):
-            self.assertEqual(tl.select_tasks('up'), ('up', 'juju'))
+            self.assertEqual(tl.select_tasks('lp'), ('lp', 'juju'))
             output = sys.stdout.getvalue().strip()
-            # self.assertEqual(output, '1) what\n2) false\n3) true\n4) ifenslave\n5) autoconf\n6) autotools\n7) make\n8) grep\n9) awk\n10) binutils\n11) sleep\n12) rsyslog\n13) mom\n14) kernel\n15) sabdfl\n16) server\n17) foundation\n18) mgr\n19) team\n20) supp\n21) juju\n22) maas\n)')
-            self.assertEqual(output, '1) what\n2) false\n3) true\n4) ifenslave\n5) autoconf\n6) autotools\n7) make\n8) grep\n9) awk\n10) binutils')
+            self.assertEqual(output, """1) what
+2) false
+3) true
+4) ifenslave
+5) autoconf
+6) autotools
+7) make
+8) grep
+9) awk
+10) binutils""")
 
     def _get_last_log_line(self):
         with open(tl.LogFile, 'r') as timelog:
